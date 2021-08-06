941971196c0fc8563570f1513567f325e356a981
Noe Osorio <osorionoe@gmail.com>
2021-08-06 12:02:22 -0500

    Day 3, Asynchronism

--- src/App.js
+++ src/App.js
@@ -7,6 +7,30 @@ function App() {
   const [login, setLogin] = useState(true); //bool
   console.log(login);
 
+  // Callback
+  setTimeout(() => {}, 5000); //5 sec
+
+  // Promise
+  const num = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(56);
+    }, 5000);
+  });
+
+  num
+    .then((value) => {
+      console.log(value);
+    })
+    .catch((err) => console.log(err));
+
+  // async/await
+  const asyncFunction = async () => {
+    const asyncNum = await num;
+    console.log({ asyncNum });
+  };
+
+  asyncFunction();
+
   const buttons = [
     <Botoncito name="Register" onClick={() => setLogin(!login)} />,
     <Botoncito name="Register" onClick={() => setLogin(!login)} />,
